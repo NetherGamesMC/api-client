6496f95f95052dcee021ee56ded75a04bbb52416
Hampus Kraft <hello@hampuskraft.com>
2022-09-04 23:01:11 +0200

    Remove old test

--- src/tests/status.test.ts
+++
@@ -1,9 +0,0 @@
-import test from 'ava'
-
-import {NetherGamesClient} from '../client.js'
-
-const client = new NetherGamesClient('testing')
-
-test('retrieving status works', async t => {
-  await t.notThrowsAsync(() => client.status.retrieve())
-})
